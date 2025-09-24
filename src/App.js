1f3152fd7bc1dbf18e7ba6d73698f55e251d3c26
AlexLessus <l22290827@cdguzman.tecnm.mx>
2025-09-23 21:33:27 -0600

    Implementacion de PrivateRoute

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import DashboardPage from './pages/DashboardPage/Dashboard';
 import SignIn from './pages/sign-in/SignIn';
@@ -12,36 +12,22 @@ import Progress from './pages/Progress/Progress';
 import Calendar from './pages/Calendar/Calendar';
 import AboutUs from './pages/AboutUs/AboutUs';
 
-// 1. Importa el proveedor de autenticación
 import { AuthProvider } from './context/AuthContext';
-
-// 2. Importa el componente de ruta protegida
 import PrivateRoute from './components/PrivateRoute';
 
 function App() {
   return (
-    // 3. Envuelve toda la aplicación con el AuthProvider
     <AuthProvider>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<AboutUs />} />
           <Route path="/signin" element={<SignIn />} />
           <Route path="/signup" element={<SignUp />} />
-          <Route path="/habits" element={<Habits />} />
-          <Route path="/tasks" element={<Tasks />} />
-          <Route path="/calendar" element={<Calendar />} />
-          <Route path="/progress" element={<Progress />} />
-
-          {/* 4. Protege la ruta del dashboard con PrivateRoute */}
-          <Route
-            path="/dashboard"
-            element={
-              <PrivateRoute>
-                <DashboardPage />
-              </PrivateRoute>
-            }
-          />
-
+          <Route path="/habits" element={<PrivateRoute><Habits /></PrivateRoute>} />
+          <Route path="/tasks" element={<PrivateRoute><Tasks /></PrivateRoute>} />
+          <Route path="/calendar" element={<PrivateRoute><Calendar /></PrivateRoute>} />
+          <Route path="/progress" element={<PrivateRoute><Progress /></PrivateRoute>} />
+          <Route path="/dashboard" element={<PrivateRoute><DashboardPage /></PrivateRoute>} />
         </Routes>
       </BrowserRouter>
     </AuthProvider>
